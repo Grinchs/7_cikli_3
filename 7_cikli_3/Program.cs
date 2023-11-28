e6764ce921d83c338ac84d6ca7ed362c12b5c860
Grinchs <marcisg2345@gmail.com>
2023-11-28 20:55:12 +0200

    Add project files.

---
+++ 7_cikli_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_cikli_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ievadiet trīsstūra platumu: ");
+            int platums = int.Parse(Console.ReadLine());
+
+            for (int rinda = platums; rinda >= 1; rinda--)
+            {
+                for (int tukšumi = 1; tukšumi <= platums - rinda; tukšumi++)
+                {
+                    Console.Write(' ');
+                }
+                for (int kolonna = 1; kolonna <= 2 * rinda - 1; kolonna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
